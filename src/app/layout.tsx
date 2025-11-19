6de269f29028303cd468b8c1c51aeb3e13944c09
unknown <you@example.com>
2025-11-19 03:56:45 -0300

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { DM_Sans as DMSans } from 'next/font/google';
 import './globals.css';
 import Transition from '@/components/transition';
@@ -18,6 +19,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={dmSans.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17040995173"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-17040995173');
+          `}
+        </Script>
         <Transition>{children}</Transition>
       </body>
     </html>
